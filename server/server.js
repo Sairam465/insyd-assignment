460ad72916a66947840643538987959584c5a32a
Basani Sairam <basanisairam7683@gmail.com>
2025-12-25 12:12:33 +0530

    Fix CORS and allow Vercel frontend access

--- server/server.js
+++ server/server.js
@@ -7,7 +7,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
 app.use(bodyParser.json());
 
 
